5fdc418c9ef976113c33f0cffcd010e5f82597f7
Mónica Nayely Flores Gómez <autumn64@disroot.org>
2024-08-10 15:12:32 -0600

    v5.1.1
    Signed-off-by: Mónica Nayely Flores Gómez <autumn64@disroot.org>

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ const tar = require('tar');
 const interfaces = require("./js/interfaces.js");
 const path = interfaces.getPath();
 const langs = require("./js/lang/languages.json");
+const route = require('path');
+
 let userinfo
 let currentlang
 
@@ -32,7 +34,7 @@ if (fs.existsSync(`${path}/config.json`)){
     currentlang = langs.misc["en"];
 }
 
-const currentVer = 510;
+const currentVer = 511;
 
 function createWindow(){
     const win = new BrowserWindow({
@@ -90,8 +92,29 @@ function createWindow(){
         app.relaunch();
         app.exit(0);
     });
+    
+    process.chdir(route.join(app.getPath("exe"), "../"));
+    if (process.argv.includes("--debugging")){
+        console.log(`Copyright (c) 2023-2024, Mónica Gómez (Autumn64)
 
-    win.removeMenu();
+RaccoonLock is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+RaccoonLock is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <https://www.gnu.org/licenses/>.\n`);
+        console.log("Debugging mode enabled. Press ctrl+shift+i to open DevTools.\n")
+        console.log(`Current dir: ${process.cwd()}\n`);
+        console.log(`Argv: ${process.argv}`)
+    } else{
+        win.removeMenu();
+    }
     win.loadFile('index.html', { query: { arguments: JSON.stringify(process.argv) } });
 }
 
